4b71fa1930c42b80d004fa8406bb068d3d6673e5
Becca Lesser <raless@umich.edu>
2016-10-15 12:04:29 -0500

    Adding webpage

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>	
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+		<link href = "bootstrap/css/bootstrap.min.ss" rel="stylesheet">
+		<script src = "bootstrap/js/bootstrap.min.js"></script>
+	</head>
+	<body>
+		<h1> Google Maps Analysis </h1>
+	</body>
+</html>
