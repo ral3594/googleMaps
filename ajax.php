ca4401c78644944698e9e227c659c3e8b786b8e1
ral3594 <raless@umich.edu>
2017-02-11 04:01:13 +0000

    Bar chart working with dynamic data from db

--- ajax.php
+++ ajax.php
@@ -8,43 +8,46 @@ $db = "googleMaps";
 $conn = new mysqli($servername, $username, $password, $db);
 
 if ($_POST['action'] == "test"){
-    $sql = "SELECT Time, toHome FROM mapsTable WHERE Time < '10:00:00';";
-    
-    $result = $conn->query($sql);
-    
-    if (!$result){
-        exit($conn->error);
-    }
-    
-    $arr = array();
-    $final = array("elts" => array());
-    while ($row = $result->fetch_assoc()){
-        $time = $row['Time'];
-        $duration = $row['toHome'];
+    if (isset($_POST['weekDay'])){
+
+        // $sql = " SELECT Time, AVG(SUBSTRING_INDEX(toHome, 'mins', 1)) AS toHome FROM mapsTable WHERE Time > '5:00:00' AND Time < '6:00:00' AND Weekday = '{$_POST['weekDay']}' GROUP BY Time;";
+        $sql = "SELECT Time, SUBSTRING_INDEX(toHome, 'mins', 1) AS toHome FROM mapsTable WHERE Time >= '6:00:00' AND Time <= '7:00:00' AND Weekday = '{$_POST['weekDay']}' ORDER BY Time ASC, Date ASC;";
+        $result = $conn->query($sql);
         
-        if (strpos($duration, 'hour') !== false) {
-            $temp = explode(" ", $duration);
-            $duration = (intval($temp[0]) * 60) + intval($temp[2]);
-        }
-        else{
-            $duration = explode(" ", $duration)[0];
+        if (!$result){
+            exit($conn->error);
         }
         
-        if (array_key_exists($time, $arr)){
-            array_push($arr[$time], $duration);
+        $arr = array();
+        $final = array("elts" => array());
+        while ($row = $result->fetch_assoc()){
+            $time = $row['Time'];
+            $duration = $row['toHome'];
+            
+            // if (strpos($duration, 'hour') !== false) {
+            //     $temp = explode(" ", $duration);
+            //     $duration = (intval($temp[0]) * 60) + intval($temp[2]);
+            // }
+            // else{
+            //     $duration = explode(" ", $duration)[0];
+            // }
+            
+            if (array_key_exists($time, $arr)){
+                array_push($arr[$time], $duration);
+            }
+            else{
+                $arr[$time] = array($duration);
+            }
         }
-        else{
-            $arr[$time] = array($time, $duration);
-        }
-    }
-    
-    foreach ($arr as $key => $value){
-        // print_r($value);
-        array_push($final["elts"], $value);
-    }
-    
-    $final['keys'] = array_keys($arr);
-    
+        
+        // foreach ($arr as $key => $value){
+        //     // print_r($value);
+        //     array_push($final["elts"], $value);
+        // }
+        $final["elts"] = $arr;
+        
+        $final['keys'] = array_keys($arr);
+    }    
 
     echo json_encode($final);
     
--- js/index.js
+++ js/index.js
@@ -1,10 +1,26 @@
 function buildChart(info){
-    var elts = JSON.stringify(info["elts"]);
-    var keys = JSON.stringify(info["keys"]);
-    
+    // var elts = JSON.stringify(info["elts"]);
+    // var keys = JSON.stringify(info["keys"]);
+    var keys = info['keys'];
+    var e = info['elts'];
+    console.log(e);
+    console.log([keys[0], e[keys[0]]]);
     var chart = c3.generate({
         data: {
-            rows: elts,
+            columns: [
+                [keys[0]].concat(e[keys[0]]),
+                [keys[1]].concat(e[keys[1]]),
+                [keys[2]].concat(e[keys[2]]),
+                [keys[3]].concat(e[keys[3]]),
+                [keys[4]].concat(e[keys[4]]),
+                [keys[5]].concat(e[keys[5]]),
+                [keys[6]].concat(e[keys[6]]),
+                [keys[7]].concat(e[keys[7]]),
+                [keys[8]].concat(e[keys[8]]),
+                [keys[9]].concat(e[keys[9]]),
+                [keys[10]].concat(e[keys[10]]),
+                [keys[11]].concat(e[keys[11]]),
+            ],
             type: 'bar'
         },
         bar: {
@@ -15,18 +31,36 @@ function buildChart(info){
             //width: 100 // this makes bar width 100px
         }
     });
+    
+    // var chart = c3.generate({
+    //     data: {
+    //         columns: [
+    //             ['data1', 30, 200, 100, 400, 150, 250],
+    //             ['data2', 130, 100, 140, 200, 150, 50]
+    //         ],
+    //         type: 'bar'
+    //     },
+    //     bar: {
+    //         width: {
+    //             ratio: 0.5 // this makes bar width 50% of length between ticks
+    //         }
+    //         // or
+    //         //width: 100 // this makes bar width 100px
+    //     }
+    // });
 
 }
 
 function loadData(){
     $.ajax({
         url: "../ajax.php", 
-        data: {action: "test"},
+        data: {action: "test", weekDay: "Monday"},
         type: "post",
         dataType: "json",
         success: function(result){
-            console.log(result["keys"]);
-            console.log(JSON.stringify(result["elts"]));
+            // console.log(result["keys"]);
+            console.log(result["elts"]);
+            // console.log(JSON.stringify(result["elts"]));
             buildChart(result);
         },
         error: function(){
